8343062d755edef479b95da8ea2ca11516d1458d
dhamark <dhamark@gmail.com>
2015-04-02 00:43:42 +0200

    2nd commit

--- assignment_1/src/main/java/com/example/david/assignment_1/MainActivity.java
+++ assignment_1/src/main/java/com/example/david/assignment_1/MainActivity.java
@@ -4,16 +4,65 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import java.util.Random;
 
 public class MainActivity extends Activity {
+
+
+    Random random = new Random();
+    int n = 0;
+
+
+    public void buttonOnClick(View v){
+
+        TextView textView(TextView) findViewById(R.id.quotes);
+
+        n = rand.nextInt(3);
+
+        String[] quotes = getResources().getStringArray(R.array.quotes);
+
+        textView.setText(quotes[n]);
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
